8bf066e47b105ac8846367c174113c5cbf239f9f
jenniferja <jenniferjaystar2@gmail.com>
2025-11-06 13:40:20 -0700

    updated hero section

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -1,4 +1,4 @@
-import { ArrowDown } from "lucide-react";
+import { ArrowDown, Github, Linkedin, Rocket } from "lucide-react";
 import { FloatingBlocks } from "./FloatingBlocks"; 
 export const HeroSection = () => {
   return (
@@ -22,8 +22,52 @@ export const HeroSection = () => {
           </h1>
 
           <p className="text-lg md:text-xl text-muted-foreground max-2-2xl mx-auto opacity-0 animate-fade-in-delay-3">
-            Driven to explore technology and solve real-world problems.
+            Always exploring, always building. <br></br>Ready for the next challenge worth solving!
           </p>
+          <div className="flex justify-center gap-4 mt-4 opacity-0 animate-fade-in-delay-4">
+            <a
+              href="https://devpost.com/jenniferjaystar2"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="group p-2 rounded-full border border-foreground/20 hover:border-primary hover:text-primary transition-all"
+            >
+              <Rocket className="w-5 h-5" />
+              <span className="absolute -bottom-8 -translate-x-1/2 whitespace-nowrap
+              rounded-md bg-white text-black text-xs px-2 py-1 opacity-0 group-hover:opacity-100 
+              transition-opacity pointer-events-none">
+              DevPost
+            </span>
+            </a>
+            <a
+              href="https://github.com/JenniferJa"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="group p-2 rounded-full border border-foreground/20 hover:border-primary hover:text-primary transition-all"
+            >
+              <Github className="w-5 h-5"/>
+              <span className="absolute -bottom-8 -translate-x-1/2 whitespace-nowrap
+              rounded-md bg-white text-black text-xs px-2 py-1 opacity-0 group-hover:opacity-100 
+              transition-opacity pointer-events-none">
+              GitHub
+            </span>
+            </a>
+
+            <a
+              href="https://www.linkedin.com/in/jennifer-jay1"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="group p-2 rounded-full border border-foreground/20 hover:border-primary hover:text-primary transition-all"
+            >
+              <Linkedin className="w-5 h-5" />
+              <span className="absolute -bottom-8 -translate-x-1/2 whitespace-nowrap
+              rounded-md bg-white text-black text-xs px-2 py-1 opacity-0 group-hover:opacity-100 
+              transition-opacity pointer-events-none">
+              Linkedin
+            </span>
+            </a>
+
+            
+          </div>
 
         </div>
       </div>
